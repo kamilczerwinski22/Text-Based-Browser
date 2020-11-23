161884b935fe3d25a84e263b167e52130a1e3fba
Kamil Czerwinski <kamilcze31@gmail.com>
2020-11-23 18:14:50 +0100

    Stage 3/6, back option

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+from collections import deque
 
 nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -42,8 +42,11 @@ ERROR_TEXT = 'FFS, Error again!'
 
 
 def crop_after_last_dot(string):
-    """Function for croping website link"""
-    return string[:len(string) - 1 - string[::-1].index('.')]
+    """Function for croping website link from last dot. If string doesn't containg dot, return input string"""
+    try:
+        return string[:len(string) - 1 - string[::-1].index('.')]
+    except ValueError:
+        return string
 
 def check_input(string):
     """Function for checking input validity"""
@@ -58,6 +61,9 @@ def write_to_file(file_name, catalog_name, file_inside):
 # MAIN LOOP
 def main():
 
+    #create stack
+    stack = deque()
+
     # create folder
     try:
         os.mkdir(catalog_name)
@@ -67,21 +73,48 @@ def main():
 
     while True:
         user_input = input()
-
+        user_input_without_last_dot = crop_after_last_dot(user_input)
         if user_input == 'exit':  # exit
             exit()
+
+        elif user_input == 'back':  # back for getting to second last read argument, e.q. if first we read A and then B,
+                                    # after typing 'back' we will see content of A again.
+            try:
+                # first pop arguments to read
+                first_arg = stack.pop()
+                second_arg = stack.pop()
+
+                # read content of second to last argument
+                with open(catalog_name + '\\' + second_arg, 'r+') as f:
+                    for line in f.readlines():
+                        print(line.strip())
+
+                # get them back in
+                stack.append(second_arg)
+                stack.append(first_arg)
+
+            except IndexError:
+                pass
+
         elif check_input(user_input):  # check input validity
             print(ERROR_TEXT)
-        elif os.path.isfile(catalog_name + '\\' + crop_after_last_dot(user_input)):  # second read
-            with open(catalog_name + '\\' + crop_after_last_dot(user_input), 'r+') as f:
+
+        elif os.path.isfile(catalog_name + '\\' + user_input_without_last_dot):  # second read of content
+            stack.append(user_input_without_last_dot)
+            with open(catalog_name + '\\' + user_input_without_last_dot, 'r+') as f:
                 for line in f.readlines():
                     print(line.strip())
+
         elif user_input == 'bloomberg.com':  # first occurence of bloomberg
             print(bloomberg_com)
             write_to_file(crop_after_last_dot('bloomberg.com'), catalog_name, bloomberg_com)
+            stack.append(crop_after_last_dot('bloomberg.com'))
+
         elif user_input == 'nytimes.com':  # first occurence of nytimes
             print(nytimes_com)
             write_to_file(crop_after_last_dot('nytimes.com'), catalog_name, nytimes_com)
+            stack.append(crop_after_last_dot('nytimes.com'))
+
         else:  # error else
             print(ERROR_TEXT)
 
